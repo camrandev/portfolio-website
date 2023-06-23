05bf89e5916d4cd5de8cd81b577ca8c16ea3c816
Crynowecer0 <crynowecer11@gmail.com>
2023-06-23 16:34:41 -0400

    add headshot and start to style

--- src/Introduction.jsx
+++ src/Introduction.jsx
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'
+import headshot from './assets/headshot.jpg'
 
  /** DESCRIPTION
 *
@@ -10,7 +11,6 @@ import React, { useState, useEffect } from 'react';
 */
 
 function Introduction () {
-
   const [textIndex, setTextIndex] = useState(0);
   const textArray = ["a Software Engineer", "a Runner", "lorem ipsum"];
 
@@ -33,10 +33,13 @@ function Introduction () {
 
   //TODO:add more friendly looking animation
   return (
-    <div>
+    <div className='d-flex'>
       <div id="showcase" className="react-reveal" style={style}><div className="showcase-content"><h1>Hello, I'm Camran</h1><h2> I am <span className="txt-rotate">
       <span>{textArray[textIndex]}</span>
     </span></h2></div></div>
+    <div className="col-md-6 mx-auto">
+      <img className="img-fluid" src={headshot} alt="" />
+    </div>
     </div>
   )
 }
